b716d0b9a3617892ed759de6f141967436a4243d
ramithakotarkonda <rkotarko@purdue.edu>
2021-02-28 05:54:19 -0600

    "asdfasdfa"

--- unilyfe_app/lib/main.dart
+++ unilyfe_app/lib/main.dart
@@ -8,7 +8,7 @@ Future main() async {
   runApp(MyApp());
 }
 
-// testing
+//testing
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   static final String title = 'Sign In';
