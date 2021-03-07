ca6f590f32cc6040204a30469de8ba5c4a78636a
Unnati Singh <unnatisingh@Unnatis-MacBook-Air-2.local>
2021-03-06 21:39:09 -0800

    changed the UI of the profile page a bit and changed it so the four channel tabs only show up on the home page

--- unilyfe_app/lib/page/home_page.dart
+++ unilyfe_app/lib/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:unilyfe_app/buttons/logout_button.dart';
 import 'package:unilyfe_app/provider/google_sign_in.dart';
+import 'package:unilyfe_app/page/profile_page.dart';
 
 class HomePage extends StatelessWidget {
   static Route<dynamic> route() => MaterialPageRoute(
@@ -11,7 +12,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
 
-    return Container(
+      return Scaffold(
+      appBar: 
+        TabBar(
+          labelColor: const Color(0xFFF56D6B),
+          tabs: [
+            Tab(text: "ALL"),
+            Tab(text: "FOOD"),
+            Tab(text: "STUDY"),
+            Tab(text: "SOCIAL"),
+          ],
+          unselectedLabelColor: Colors.grey,
+        ),
+      //),
+      body: TabBarView(
+        children: [
+          //Icon(Icons.directions_car),
+          Text("feedfeedfeed", textAlign: TextAlign.center,),
+          Text("yummy nomnom", textAlign: TextAlign.center,),
+          Text("studywuddy", textAlign: TextAlign.center,),
+          Text("fun fun fun", textAlign: TextAlign.center,),
+        ],
+      ),
+      bottomSheet: LogoutButtonWidget(),
+      
+    );
+
+    /*return Container(
       alignment: Alignment.center,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -25,7 +52,7 @@ class HomePage extends StatelessWidget {
           LogoutButtonWidget(),
         ],
       ),
-    );
+    );*/
     /*return Scaffold(
       body: Center(
         child: Text("Hello, hi!"),
