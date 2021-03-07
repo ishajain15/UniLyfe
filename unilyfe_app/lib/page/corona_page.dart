8c5dbba103f8a7899cbcc3ec4c9ae0728aff427a
Unnati Singh <unnatisingh@Unnatis-MacBook-Air-2.local>
2021-03-06 21:58:55 -0800

    small change just took stuff off the corona page bc it had all the home page stuff on it

--- unilyfe_app/lib/page/corona_page.dart
+++ unilyfe_app/lib/page/corona_page.dart
@@ -8,32 +8,8 @@ class CoronaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: //AppBar(
-        /*leading: new IconButton(
-          color: Colors.grey,
-          icon: new Icon(Icons.reorder),
-          //onPressed: () => _scaffoldKey.currentState.openDrawer(),
-        ),*/
-        //title: Image.asset('assets/unilyfe_logo.png', width: 300, height: 55),
-        //backgroundColor: const Color(0xFFFFFFFF),
-        TabBar(
-          labelColor: const Color(0xFFF56D6B),
-          tabs: [
-            Tab(text: "ALL"),
-            Tab(text: "FOOD"),
-            Tab(text: "STUDY"),
-            Tab(text: "SOCIAL"),
-          ],
-          unselectedLabelColor: Colors.grey,
-        ),
-      //),
-      body: TabBarView(
-        children: [
-          Icon(Icons.directions_car),
-          Icon(Icons.directions_transit),
-          Icon(Icons.directions_bike),
-          Icon(Icons.directions_bike),
-        ],
+      body: Center(
+        child: Text("ayo its coronatime"),
       ),
     );
   }
