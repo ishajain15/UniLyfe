ef0b8fb106c1ab0db5a290a009d2b950e5d7ed3b
ramithakotarkonda <rkotarko@purdue.edu>
2021-03-07 04:26:18 -0600

    tried merging

--- unilyfe_app/lib/main.dart
+++ unilyfe_app/lib/main.dart
@@ -21,3 +21,31 @@ class MyApp extends StatelessWidget {
         home: StartPage(),
       );
 }
+// class _MyMapState extends State<MyMap> {
+//   GoogleMapController mapController;
+
+//   final LatLng _center = const LatLng(40.42395040517343, -86.92120533110851);
+
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('COVID-19 Tracker'),
+//           backgroundColor: Colors.green[700],
+//         ),
+//         body: GoogleMap(
+//           onMapCreated: _onMapCreated,
+//           initialCameraPosition: CameraPosition(
+//             target: _center,
+//             zoom: 11.0,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// } 
