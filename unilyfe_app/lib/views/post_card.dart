0eedcd5b2e2254fbd3d0de6f8098c291dc2fbbbe
Carolyn Chen <cgc731@gmail.com>
2021-03-09 18:18:44 -0800

    Updated UI of post list

--- unilyfe_app/lib/views/post_card.dart
+++ unilyfe_app/lib/views/post_card.dart
@@ -10,9 +10,20 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 5 / 2,
+      aspectRatio: 6 / 3,
       child: Card(
-        child: Column(children: <Widget>[_Post(), _PostDetails()]),
+        elevation: 2,
+        child: Container(
+          margin: const EdgeInsets.all(4.0),
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            children: <Widget>[
+              _Post(),
+              Divider(color: Colors.grey),
+              _PostDetails(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -44,13 +55,17 @@ class _PostTitleAndSummary extends StatelessWidget {
 
     return Expanded(
       flex: 3,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text(title, style: titleTheme),
-          Text(summary, style: summaryTheme),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(left: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(title, style: titleTheme),
+            SizedBox(height: 2.0),
+            Text(summary, style: summaryTheme),
+          ],
+        ),
       ),
     );
   }
@@ -71,7 +86,10 @@ class _PostDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: <Widget>[_UserImage(), _UserNameAndEmail()],
+      children: <Widget>[
+        _UserImage(),
+        _UserNameAndEmail(),
+      ],
     );
   }
 }
@@ -81,15 +99,22 @@ class _UserNameAndEmail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle nameTheme = Theme.of(context).textTheme.subtitle;
+    final TextStyle emailTheme = Theme.of(context).textTheme.body1;
+
     return Expanded(
-      flex: 7,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(DemoValues.userName),
-          Text(DemoValues.userEmail),
-        ],
+      flex: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(DemoValues.userName, style: nameTheme),
+            SizedBox(height: 2.0),
+            Text(DemoValues.userEmail, style: emailTheme),
+          ],
+        ),
       ),
     );
   }
