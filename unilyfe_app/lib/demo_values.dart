5fcc33ec4b8318e9ad836eca1aee3ec90cd42688
Carolyn Chen <cgc731@gmail.com>
2021-03-10 12:36:36 -0800

    uploaded COVID tracker legend

--- unilyfe_app/lib/demo_values.dart
+++ unilyfe_app/lib/demo_values.dart
@@ -4,6 +4,6 @@ class DemoValues {
   static final String userName = 'Carolyn Chen';
   static final String userEmail = 'cgc731@gmail.com';
   static final String postTime = 'July 31, 2021';
-  static final String postTitle = 'Test Post';
+  static final String postTitle = 'Free Cookies in Earhart!';
   static final String postSummary = 'Hello Unnati Singh gotta keep an eye out for Selener and Pancakes';
 }
