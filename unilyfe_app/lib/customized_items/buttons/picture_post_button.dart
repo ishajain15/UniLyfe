2c99248e808a2896c385f527a3997e77b6d216da
Isha <ishajainpdx@gmail.com>
2021-03-11 16:01:19 -0500

    Connected Image Picker to 'Create Picture Post' Button

--- unilyfe_app/lib/customized_items/buttons/picture_post_button.dart
+++ unilyfe_app/lib/customized_items/buttons/picture_post_button.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:unilyfe_app/page/create_posts/image_picker.dart';
 // //import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // //import 'package:unilyfe_app/page/start_page.dart';
 // import 'package:unilyfe_app/page/tabs/tabs_page.dart';
-
 
 class PictureButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
         padding: EdgeInsets.all(4),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            MaterialPageRoute(
+              builder: (context) => MyHomePage(),
+            );
+          },
           style: ElevatedButton.styleFrom(
             primary: Color(0xFFF46C6B),
             onPrimary: Colors.white,
