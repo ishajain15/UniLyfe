2d0bc9f7c63724049851f6bd351fca5cdc222317
Carolyn Chen <cgc731@gmail.com>
2021-03-24 14:18:12 -0700

    added info page for point system rules

--- unilyfe_app/lib/page/tabs/tabs_page.dart
+++ unilyfe_app/lib/page/tabs/tabs_page.dart
@@ -151,6 +151,25 @@ class _TabsPageState extends State<TabsPage> {
               //SearchPage();
             },
           ),
+          ListTile(
+            title: Text(
+              'Point System',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+                fontFamily: 'Raleway',
+                fontSize: 17,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PointsRules()),
+              );
+              print('here');
+              //SearchPage();
+            },
+          ),
         ],
       ),
     );
@@ -311,6 +330,54 @@ class AboutApp extends StatelessWidget {
            "there are other existing online review applications such as Yelp or Tripadvisor, none of them are " +
           "geared solely towards college students. UniLyfe will stand out because of its ability to provide " +
            "personalized recommendations for locations on university campuses.", textAlign: TextAlign.center,
+          style: const TextStyle(
+          fontFamily: 'Raleway',
+                color: Colors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                ),
+                ),
+        ],
+      ),
+    )
+    );
+  }
+}
+
+class PointsRules extends StatelessWidget {
+  @override
+
+    Widget pad = Container(
+    padding: const EdgeInsets.all(16),
+  );
+
+  Widget build(BuildContext context) {
+     double c_width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFFFFFF),
+        title: Text(
+          'Point System Rules',
+          style: TextStyle(color: Colors.grey),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.grey, //change your color here
+        ),
+      ),
+      body: 
+   Container (
+      padding: const EdgeInsets.all(32.0),
+      width: c_width,
+      child: Column (
+        children: <Widget>[
+          Text('Point System Rules', textAlign: TextAlign.left,
+          style: const TextStyle(
+          fontFamily: 'Raleway',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                ),
+                ),
+         Text('You will earn 5 points for posting a text post detailing your experience at a place.', textAlign: TextAlign.center,
           style: const TextStyle(
           fontFamily: 'Raleway',
                 color: Colors.grey,
