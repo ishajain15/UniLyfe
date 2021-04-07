3fe30187689ef593e8c558e9f9bc7422d27e8584
Isha <ishajainpdx@gmail.com>
2021-04-07 14:55:37 -0400

    Added app bar to create poll post page

--- unilyfe_app/lib/page/create_posts/poll_form.dart
+++ unilyfe_app/lib/page/create_posts/poll_form.dart
@@ -3,152 +3,169 @@ import 'package:unilyfe_app/widgets/provider_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:unilyfe_app/models/poll_post.dart';
 import 'package:unilyfe_app/models/global.dart' as global;
+
 int selection = 0;
+
 // ignore: must_be_immutable
 class PollForm extends StatelessWidget {
   final db = FirebaseFirestore.instance;
-  String _question, _option1,_option2,_option3,_option4;
+  String _question, _option1, _option2, _option3, _option4;
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        TextField(
-            decoration: InputDecoration(
-                hintText: 'Question',
-                contentPadding:
-                    const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.7),
-            ),
-            ),
-            onChanged: (value) {_question = value.trim();},
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
         ),
-        TextField(
-            decoration: InputDecoration(
-                hintText: 'Option 1',
-                contentPadding:
-                    const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.7),
-            ),
-            ),
-            onChanged: (value) {_option1 = value.trim();},
+        title: Text(
+          'CREATE A POLL POST',
+          style: TextStyle(color: Colors.black),
         ),
-        TextField(
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
             decoration: InputDecoration(
-                hintText: 'Option 2',
-                contentPadding:
-                    const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.7),
+              hintText: 'Question',
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
             ),
-            ),
-            onChanged: (value) {_option2 = value.trim();},
-        ),
-        TextField(
+            onChanged: (value) {
+              _question = value.trim();
+            },
+          ),
+          TextField(
             decoration: InputDecoration(
-                hintText: 'Option 3',
-                contentPadding:
-                    const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.7),
+              hintText: 'Option 1',
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
             ),
-            ),
-            onChanged: (value) {_option3 = value.trim();},
-        ),
-         TextField(
+            onChanged: (value) {
+              _option1 = value.trim();
+            },
+          ),
+          TextField(
             decoration: InputDecoration(
-                hintText: 'Option 4',
-                contentPadding:
-                    const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(25.7),
+              hintText: 'Option 2',
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
             ),
+            onChanged: (value) {
+              _option2 = value.trim();
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Option 3',
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
             ),
-            onChanged: (value) {_option4 = value.trim();},
-        ),
-        MyAppOne(),
-        ElevatedButton(
-                onPressed: () async {
-                  var channel = 'Post';
-                   if (selection == 0) {
-                    channel= 'FOOD';
-                  } else if (selection == 1) {
-                    channel = 'STUDY';
-                  } else {
-                    print(selection);
-                    channel = 'SOCIAL';
-                  }
+            onChanged: (value) {
+              _option3 = value.trim();
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Option 4',
+              contentPadding:
+                  const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(25.7),
+              ),
+            ),
+            onChanged: (value) {
+              _option4 = value.trim();
+            },
+          ),
+          MyAppOne(),
+          ElevatedButton(
+            onPressed: () async {
+              var channel = 'Post';
+              if (selection == 0) {
+                channel = 'FOOD';
+              } else if (selection == 1) {
+                channel = 'STUDY';
+              } else {
+                print(selection);
+                channel = 'SOCIAL';
+              }
 
+              final uid = await Provider.of(context).auth.getCurrentUID();
+              var doc = db.collection('posts').doc();
+              //  final PollPost post = new PollPost(_question, DateTime.now(), _option1, true, "Food", uid);
+              final post = PollPost(doc.id, _question, DateTime.now(), _option1,
+                  channel, uid, 0, false, {uid: false}, null);
 
-                 final uid = await Provider.of(context).auth.getCurrentUID();
-                 var doc = db.collection('posts').doc();
-                //  final PollPost post = new PollPost(_question, DateTime.now(), _option1, true, "Food", uid);
-                 final post = PollPost(doc.id,_question, DateTime.now(), _option1, channel, uid, 0,false,{uid: false}, null);
+              post.postid = doc.id;
+              global.question = _question;
+              global.option1 = _option1;
+              global.option2 = _option2;
+              global.option3 = _option3;
+              global.option4 = _option4;
 
-                  post.postid = doc.id;
-                  global.question = _question;
-                  global.option1 = _option1;
-                  global.option2 = _option2;
-                   global.option3 = _option3;
-                    global.option4 = _option4;
+              await db.collection('userData').doc(uid).get().then((result) {
+                post.username = result['username'];
+              });
 
-                    await db
-                    .collection('userData')
-                    .doc(uid)
-                    .get()
-                    .then((result) {
-                  post.username = result['username'];
-                });
-                    
-                  //DocumentReference channel;
-                  if (selection == 0) {
-                    //await db.collection("food_posts").add(post.toJson());
-                    await db
-                        .collection('food_posts')
-                        .doc(doc.id)
-                        .set(post.toJson());
-                  } else if (selection == 1) {
-                    //await db.collection("study_posts").add(post.toJson());
-                    await db
-                        .collection('study_posts')
-                        .doc(doc.id)
-                        .set(post.toJson());
-                  } else {
-                    //await db.collection("social_posts").add(post.toJson());
-                    await db
-                        .collection('social_posts')
-                        .doc(doc.id)
-                        .set(post.toJson());
-                  }
-                  await db.collection('posts').doc(doc.id).set(post.toJson());
-                  await db
-                      .collection('userData')
-                      .doc(uid)
-                      .collection('poll_posts')
-                      .doc(doc.id)
-                      .set(post.toJson());
-                  //  Navigator.of(context).popUntil((route) => route.isFirst);
-                //  Navigator.of(context).popUntil((route) => route.isFirst);
-                Navigator.pop(context);
-                  // Navigator.pushReplacement(context,MaterialPageRoute(builder: (_) => CreatePage())).then((_) => refresh());
-              },
+              //DocumentReference channel;
+              if (selection == 0) {
+                //await db.collection("food_posts").add(post.toJson());
+                await db
+                    .collection('food_posts')
+                    .doc(doc.id)
+                    .set(post.toJson());
+              } else if (selection == 1) {
+                //await db.collection("study_posts").add(post.toJson());
+                await db
+                    .collection('study_posts')
+                    .doc(doc.id)
+                    .set(post.toJson());
+              } else {
+                //await db.collection("social_posts").add(post.toJson());
+                await db
+                    .collection('social_posts')
+                    .doc(doc.id)
+                    .set(post.toJson());
+              }
+              await db.collection('posts').doc(doc.id).set(post.toJson());
+              await db
+                  .collection('userData')
+                  .doc(uid)
+                  .collection('poll_posts')
+                  .doc(doc.id)
+                  .set(post.toJson());
+              //  Navigator.of(context).popUntil((route) => route.isFirst);
+              //  Navigator.of(context).popUntil((route) => route.isFirst);
+              Navigator.pop(context);
+              // Navigator.pushReplacement(context,MaterialPageRoute(builder: (_) => CreatePage())).then((_) => refresh());
+            },
             //   child: Text("Post"),
             //  onPressed: () async{
-               child: Text('SUBMIT'),
-            ),
-      ],
-    ),
-    
-  );
-}
+            child: Text('SUBMIT'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class MyAppOne extends StatefulWidget {
