56eca8850f1a10e9e21138368307f9a107aa2a36
Unnati Singh <unnatisingh@Unnatis-MacBook-Air-2.local>
2021-04-13 20:13:52 -0700

    created basic card for friend

--- unilyfe_app/lib/views/friend_card.dart
+++ unilyfe_app/lib/views/friend_card.dart
@@ -26,6 +26,100 @@ class FriendCard extends StatelessWidget {
     );
   }*/
   Widget build(BuildContext context) {
-    return Container();
+    return AspectRatio(
+      aspectRatio: 4 / 3,
+      child: Card(
+        elevation: 2,
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            children: [
+              Expanded(
+                /*1*/
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /*2*/
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Potential Friend',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Sophomore',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: Text(
+                        'user biooooooo',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Also takes:',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Also likes:',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              /*3*/
+              CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/empty-profile.png')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _FriendImage extends StatelessWidget {
+  const _FriendImage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(flex: 2, child: Image.asset(DemoValues.postImage));
   }
 }
