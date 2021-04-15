fe670c70d1374ff112c4e14fe1e4aa8a25de9da7
Unnati Singh <unnatisingh@Unnatis-MacBook-Air-2.local>
2021-04-15 13:58:12 -0700

    buttons work now

--- unilyfe_app/lib/views/friend_card.dart
+++ unilyfe_app/lib/views/friend_card.dart
@@ -432,50 +432,95 @@ class myAddFriendButton extends State<addFriendButtonWidget> {
   bool addFriendBool = true;
   bool addedBool = false;
   String addFriend = 'Add Friend!';
-  String added = 'Added!';
+  String added = 'Friend Added!';
   String friendUIDLocal = friendUID;
+  var friendsList = [];
+  int stateSet = 0;
+
+  /*
+        FutureBuilder(
+          future: Provider.of(context).auth.getCurrentUID(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return _displayClasses(context, snapshot);
+            } else {
+              return buildLoading();
+            }
+          },
+        ),
+  */
+
+  getFriendState() async {
+    String uid = await Provider.of(context).auth.getCurrentUID();
+    await db
+        .collection('userData')
+        .doc(uid)
+        .collection('friends')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        //titlesList[index] = (Text(doc['title'].toString()));
+        var friend = doc['friendUID'].toString();
+        //titlesList.add(doc['title'].toString());
+        friendsList.add(friend);
+      });
+    });
+    if (friendsList.contains(friendUIDLocal) && stateSet == 0) {
+      print(friendUIDLocal);
+      //addFriendBool = !addFriendBool;
+      //addedBool = !addedBool;
+      addFriendBool = false;
+      addedBool = true;
+    }
+    stateSet = 0;
+  }
 
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(4),
-      child: ElevatedButton(
-        onPressed: () async {
-          String uid = await Provider.of(context).auth.getCurrentUID();
-          print("friendUID: " + friendUIDLocal);
-
-          if (addFriendBool) {
-            await db
-                .collection('userData')
-                .doc(uid)
-                .collection('friends')
-                .doc(friendUIDLocal)
-                .set({'friendUID': friendUIDLocal});
-          } else {
-            await db
-                .collection('userData')
-                .doc(uid)
-                .collection('friends')
-                .doc(friendUIDLocal)
-                .delete();
-          }
-          setState(() {
-            addFriendBool = !addFriendBool;
-            // print("addFriendBool: " + addFriendBool.toString());
-            addedBool = !addedBool;
-            // print("addedBool: " + addedBool.toString());
-            //print('on press: the randomized button has been clicked');
-          });
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Color(0xFFF46C6B),
-          onPrimary: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        ),
-        child: Text(addFriendBool ? addFriend : added
-            //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    //return Container(
+    return FutureBuilder(
+        future: getFriendState(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {}
+          return Container(
+            padding: EdgeInsets.all(4),
+            child: ElevatedButton(
+              onPressed: () async {
+                String uid = await Provider.of(context).auth.getCurrentUID();
+                if (addFriendBool) {
+                  await db
+                      .collection('userData')
+                      .doc(uid)
+                      .collection('friends')
+                      .doc(friendUIDLocal)
+                      .set({'friendUID': friendUIDLocal});
+                } else {
+                  await db
+                      .collection('userData')
+                      .doc(uid)
+                      .collection('friends')
+                      .doc(friendUIDLocal)
+                      .delete();
+                }
+                setState(() {
+                  addFriendBool = !addFriendBool;
+                  // print("addFriendBool: " + addFriendBool.toString());
+                  addedBool = !addedBool;
+                  stateSet = 1;
+                  // print("addedBool: " + addedBool.toString());
+                  //print('on press: the randomized button has been clicked');
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFF46C6B),
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+              child: Text(addFriendBool ? addFriend : added
+                  //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
             ),
-      ),
-    );
+          );
+        });
   }
 }
